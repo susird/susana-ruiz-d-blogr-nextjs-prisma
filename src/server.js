f87deb7865f4dd960be056e61223c3b687bc8763
susana <susir95@gmail.com>
2021-10-03 19:44:49 -0500

    add apollo to nextjs

--- src/server.js
+++ src/server.js
@@ -4,18 +4,26 @@ const { PrismaClient }= require('@prisma/client');
 const prisma = new PrismaClient();
 const port = process.env.PORT || 8080
 const typeDefs = `
+  type Post {
+    title: String
+    content: String
+    author: User
+  }
   type User {
     email: String!
     name: String
   }
   type Query {
     allUsers: [User!]!
-  }
-`;
+    allPosts: [Post!]!
+  }`;
 const resolvers = {
   Query: {
     allUsers: () => {
       return prisma.user.findMany();
+    },
+    allPosts: () => {
+      return prisma.post.findMany();
     }
   }
 };
